6fdc55caefd63bc59c474f8caafcb5a32219e3aa
abdullohasia <tolaganova624@gmail.com>
2023-10-09 18:16:39 +0500

    Abdulloh's project

---
+++ index.js
@@ -0,0 +1,549 @@
+//masala 
+
+//  let alisher = parseInt(prompt("Alisher pulingni kirit !!!"))
+//  let bilet = 500
+//  let atel = 250
+//  let muzey = 120
+//  let harajat = 120 * 12354.03 + 11000.34 * ( 500 + 250 )
+//  alert(harajat)
+//  console.log(harajat, ("hamma harajatlar"))
+//  if (alisher > harajat ) {
+//     console.log("Alisher sanga oq yol puling yetadi")
+//     alert( ("Alisher sanga oq yol puling yetadi"),)
+//  } else if (alisher < harajat ) {
+//     console.log("Alisher damingni olib tur hali sanga bor")
+//     alert(("Alisher damingni olib tur hali sanga bor"))
+//  }
+
+
+
+//if_1
+
+//  let son = parseInt(prompt("sonni kiriting"))
+//  if ( son > 0) {
+//     alert(++son)
+//  } else if (son < 0) {
+//    alert(false)
+
+
+
+//if_2 
+
+//  let son = parseInt(prompt("sonni kiriting"))
+//  if ( son > 0) {
+//     alert(++son)
+//  } else if (son < 0) {
+//    alert(son - 2)
+//    console.log(son - 2)
+//  }
+
+
+
+//if_3
+
+//  let son = parseInt(prompt("sonni kiriting"))
+//  if ( son > 0) {
+//     alert(++son)
+//  } else if (son < 0) {
+//     alert(son - 2)
+//  } else if (son == 0) {
+//     alert(10)
+//  }
+
+
+
+//if_4
+
+// let a = parseInt(prompt("Birinchi sonni kiriting"))
+// let b = parseInt(prompt("Ikkinchi sonni kiriting"))
+// let c = parseInt(prompt("Uchinchi sonni kiriting"))
+// if (a > 0 , b > 0 , c > 0) {
+//   alert("Uchta musbat son bor")
+// }  else  {
+//   alert("False")
+// } 
+// if ( a < 0 , b > 0 , c > 0 )   {
+//   alert("Ikkita musbat son bor")
+// } else {
+//   alert("False")
+// }
+// if ( a > 0 , b < 0 , c > 0 )   {
+//   alert("Ikkita musbat son bor")
+// } else {
+//   alert("False")
+// }
+// if ( a > 0 , b > 0 , c < 0 )   {
+//   alert("Ikkita musbat son bor")
+// } else {
+//   alert("False")
+// }
+
+
+//if_5 && SAVOL
+
+
+//if_6
+
+// const a = parseInt(prompt("Birnichi sonni kiriting")) 
+// const b = parseInt(prompt("Ikkinchi sonni kiriting")) 
+// if ( a > b ) {
+//   alert(a)
+// } else{
+//   alert(b)
+// }
+
+
+
+//if_7 
+
+// const a = parseInt(prompt("Birnichi sonni kiriting")) 
+// const b = parseInt(prompt("Ikkinchi sonni kiriting")) 
+// if ( a < b ) {
+//   alert(a)
+// } else{
+//   alert(b)
+
+
+
+//if_8
+
+// const a = parseInt(prompt("Birnichi sonni kiriting"))
+// const b = parseInt(prompt("Ikkinchi sonni kiriting"))
+// if ( a > b ) {
+//   alert(a)
+// } else {
+//   alert(b)
+// }
+
+
+
+//if_9 
+
+// const a = parseInt(prompt("a sonni kiriting. b dan kichik bo'lsin"))
+// const b = parseInt(prompt("b sonni kiritin"))
+// if ( a < b ) {
+//   alert(a)
+//   alert(b)
+// } else {
+//   alert(false)
+// }
+
+
+
+//if_10
+
+// const a = parseInt(prompt("a sonni kiriting"))
+// const b = parseInt(prompt("b sonni kiritin"))
+// if (a != b) {
+//   alert(a+b)
+// } else {
+//   alert(a)
+//   alert(b)
+// }
+
+
+
+//if_11
+
+//  const a = parseInt(prompt("a sonni kiriting"))
+//  const b = parseInt(prompt("b sonni kiritin"))
+//  if (a != b) {
+//   if ( a > b ) {
+//     alert(a) 
+//   } else {
+//     alert(b)
+//   }
+//  } else {
+//   alert(0)
+//  }
+
+
+
+//if_12 
+
+// const a = parseInt(prompt("a sonni kiriting"))
+// const b = parseInt(prompt("b sonni kiriting"))
+// const c = parseInt(prompt("c sonni kiriting"))
+// if ( a < b && b < c ) {
+//   alert( a )
+// } else  {
+// if ( b < a && a < c ) {
+//   alert( b )
+//    } else {
+// if ( c < b && b < a ) {
+//   alert( c )
+//     } else {
+// if ( b < c && c < a ) {
+//   alert( b )
+// } if ( c < b && b < a ) {
+//    alert( c )
+//       } else {
+//         alert(a)
+//      }
+//     }
+//    }
+//   }
+
+
+
+ //if_13
+
+//  const a = parseInt(prompt("a sonni kiriting"))
+//  const b = parseInt(prompt("b sonni kiriting"))
+//  const c = parseInt(prompt("c sonni kiriting"))
+//  if ( a < b && b < c ) {
+//    alert( b )
+//  } else  {
+//  if ( b < a && a < c ) {
+//    alert( a )
+//     } else {
+//  if ( c < b && b < a ) {
+//    alert( b )
+//      } else {
+//  if ( b < c && c < a ) {
+//    alert( c )
+//  } if ( c < b && b < a ) {
+//     alert( b )
+//        } else {
+//          alert(c)
+//       }
+//      }
+//     }
+//    }
+
+
+
+// if_14
+
+//  const a = parseInt(prompt("a sonni kiriting"))
+//  const b = parseInt(prompt("b sonni kiriting"))
+//  const c = parseInt(prompt("c sonni kiriting"))
+//  if ( a < b && b < c ) {
+//    alert( a )
+//    alert( c )
+//  } else  {
+//  if ( b < a && a < c ) {
+//    alert( b )
+//    alert( c )
+//     } else {
+//  if ( c < b && b < a ) {
+//    alert( c )
+//    alert( a )
+//      } else {
+//  if ( b < c && c < a ) {
+//    alert( b )
+//    alert( a )
+//  } if ( c < b && b < a ) {
+//     alert( c )
+//     alert( a )
+//        } else {
+//         alert( a )
+//         alert( c )
+//       }
+//      }
+//     }
+//    }
+
+
+
+// if_15 
+
+//  const a = parseInt(prompt("a sonni kiriting"))
+//  const b = parseInt(prompt("b sonni kiriting"))
+//  const c = parseInt(prompt("c sonni kiriting"))
+//  if ( a < b && b < c ) {
+//    alert( c + b )
+//  } else  {
+//  if ( b < a && a < c ) {
+//    alert( a + c )
+//     } else {
+//  if ( c < b && b < a ) {
+//    alert( a + b )
+//      } else {
+//  if ( b < c && c < a ) {
+//    alert( a + c )
+//  } if ( c < b && b < a ) {
+//     alert( a + b )
+//        } else {
+//         alert( c + b )
+//       }
+//      }
+//  const a = parseInt(prompt("a sonni kiriting"))
+//  const b = parseInt(prompt("b sonni kiriting"))
+//  const c = parseInt(prompt("c sonni kiriting"))
+//  if ( a < b && b < c ) {
+//    alert( a )
+//    alert( c )
+//  } else  {
+//  if ( b < a && a < c ) {
+//    alert( b )
+//    alert( c )
+//     } else {
+//  if ( c < b && b < a ) {
+//    alert( c )
+//    alert( a )
+//      } else {
+//  if ( b < c && c < a ) {
+//    alert( b )
+//    alert( a )
+//  } if ( c < b && b < a ) {
+//     alert( c )
+//     alert( a )
+//        } else {
+//         alert( a )
+//         alert( c )
+//       }
+//      }
+//     }
+//    }//  const a = parseInt(prompt("a sonni kiriting"))
+//  const b = parseInt(prompt("b sonni kiriting"))
+//  const c = parseInt(prompt("c sonni kiriting"))
+//  if ( a < b && b < c ) {
+//    alert( a )
+//    alert( c )
+//  } else  {
+//  if ( b < a && a < c ) {
+//    alert( b )
+//    alert( c )
+//     } else {
+//  if ( c < b && b < a ) {
+//    alert( c )
+//    alert( a )
+//      } else {
+//  if ( b < c && c < a ) {
+//    alert( b )
+//    alert( a )
+//  } if ( c < b && b < a ) {
+//     alert( c )
+//     alert( a )
+//        } else {
+//         alert( a )
+//         alert( c )
+//       }
+//      }
+//     }
+//    }
+
+
+
+// Boolean_1
+
+// const a = parseInt(prompt("a sonni kiriting"))
+// if ( a > 0 ) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+
+
+// Boolean_2
+
+// const a = parseInt(prompt("Raqamni kiriting"))
+// if (a % 2 == 1) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean_3
+
+// const a = parseInt(prompt("Raqamni kiriting"))
+// if (a % 2 == 0) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+
+
+// Boolean_4 
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// if ( a > 2 && b <= 3) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+
+
+// Boolean_5
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// if ( a ≥ 0 && b < -2 ) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+
+
+// Boolean_6
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// const c = parseInt(prompt(" c ni kiriting "))
+// if ( a ≤ b && b ≤ c ) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+
+
+// Boolean_7
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// const c = parseInt(prompt(" c ni kiriting "))
+// if ( a > b && b > c ) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+
+
+// Boolean_8 
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// if ( a % 2 == 1 || b % 2 == 1 ) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+
+
+// Boolean_10 SAVOL
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// if ( a % 2 == 1 && b % 2 == 0 ) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+// if( a % 2 == 0 && b % 2 == 1 ) {
+//   alert(true)
+// } else 
+//  {
+//   alert(false)
+//  }
+
+
+// Boolean_11 SAVOL
+
+
+
+// Boolean_12
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// const c = parseInt(prompt(" c ni kiriting "))
+// if ( a > 0 && b > 0 && c > 0 ) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+
+
+// Boolean_13
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// const c = parseInt(prompt(" c ni kiriting "))
+// if ( a > 0 || b > 0 || c > 0 ) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+
+
+// Boolean_14
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// const c = parseInt(prompt(" c ni kiriting "))
+// if ( a < 0 && b < 0 && c > 0  || a > 0 && b < 0 && c < 0 || a < 0 && b > 0 && c < 0) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+
+
+// Boolean_15
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// const c = parseInt(prompt(" c ni kiriting "))
+// if ( a < 0 && b < 0 && c > 0  || a > 0 && b < 0 && c < 0 || a < 0 && b < 0 && c > 0) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+
+
+// Boolean_16 
+
+//  const a = parseInt(prompt(" Ikki xonali juft son kiriting "))
+//  if ( a > 9 && a < 100 && a % 2 == 0 )  {
+//   alert(true)
+//  } else {
+//   alert(false)
+//  }
+
+
+
+// Boolean_17
+
+//  const a = parseInt(prompt(" Uch xonali toq son kiriting "))
+//  if ( a < 1000 && a > 99 && a % 2 == 1 )  {
+//   alert(true)
+//  } else {
+//   alert(false)
+//  }
+
+
+
+// Boolean_18
+
+// const a = parseInt(prompt(" a ni kiriting "))
+// const b = parseInt(prompt(" b ni kiriting "))
+// const c = parseInt(prompt(" c ni kiriting "))
+// if ( a == b || b == c || c == a ) {
+//   alert(true)
+// } else {
+//   alert(false)
+// }
+
+// Boolean_19
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
